6fc787a0f4b871bf8f69ea654f2f835c3dd39db3
yourUsername <your_email@example.com>
2017-04-21 09:27:42 -0400

    Calculator Challenge attempt

--- lib/calc.rb
+++ lib/calc.rb
@@ -1,3 +1,34 @@
 # frozen_string_literal: true
 
-class Calculator; end
+class Calculator
+  def initialize(value=0)
+    @value = value
+    @num1 = num1
+    @num2 = num2
+  end
+  def add()
+    #@value + value
+    @num1 + @num2
+  end
+  def subtract()
+    #@value - value
+    @num1 - @num2
+  end
+  def multiply()
+    #@value * value
+    @num1 * @num2
+  end
+  def divide()
+    #@value / value
+    @num1 / @num2
+  end
+end
+
+
+equals #method
+  addVal = Calculator.new(num1,num2)
+  addVal.add
+
+
+
+clear #method
